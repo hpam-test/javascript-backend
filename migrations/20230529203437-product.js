e17b1e8d6e3f6f53ca26ac094a1418cece2c28f1
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2023-05-30 18:05:28 +0700

    feat(migration): created migration files using sql file

---
+++ migrations/20230529203437-product.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const sqlFile = fs.readFileSync(path.resolve(__dirname, '../database/product.sql'), 'utf8');
+
+    // Execute the SQL queries
+    return queryInterface.sequelize.query(sqlFile);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('products');
+  }
+};
+
+
+
